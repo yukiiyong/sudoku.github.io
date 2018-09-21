2e56c79ce76f9ee12383ca38783c176da1207e96
yukiiyong <911711448@qq.com>
2018-09-22 01:33:13 +0800

    Update generator.js

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -14,6 +14,7 @@ class Generator {
 
 	internalGenerate() {
 		this.martix = Toolkit.martix.makeMartix()
+		//生成一个数字顺序排列的方阵，然后每一行数字打乱顺序
 		this.orders = Toolkit.martix.makeMartix()
 						.map(row => row.map((v, i) => i))
 						.map(row => Toolkit.martix.shuffle(row))
@@ -27,6 +28,7 @@ class Generator {
 	}
 
 	fillNum(n) {
+		//填充数字n 第二个参数是行序号
 		return this.fillRow(n, 0) 
 	}
 
@@ -52,7 +54,7 @@ class Generator {
 			}
 
 			rows[colIndex] = n
-
+			//填完则进行下一行判断
 			if(!this.fillRow(n, rowIndex + 1)) {
 				rows[colIndex] = 0 
 				continue
